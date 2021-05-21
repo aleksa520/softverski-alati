4b9ea0aa7d8377350b88a88463c1eddfd5948da5
Aleksa Pavlovic <ap20160012@gmail.com>
2021-05-21 15:09:13 +0200

    added dependencies

--- backend/project.clj
+++ backend/project.clj
@@ -3,8 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :main ^:skip-aot backend.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [metosin/compojure-api "2.0.0-alpha30"]
+                 [korma "0.4.3"]
+                 [mysql/mysql-connector-java "8.0.12"]
+                 [ring/ring "1.6.3"]
+                ]
+    :ring {:handler handler.handler/app}
+    :profiles {:dev {
+                      :dependencies [[ring/ring-mock "0.4.0"]]
+                      :plugins [[lein-ring "0.12.5"]]}}
+    :repl-options {:init-ns backend.core})
